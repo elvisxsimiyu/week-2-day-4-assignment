623e884c422a79f2d81942800da97894590484ca
Elvis Simiyu <simiyu66@gmail.com>
2026-09-15 16:34:47 +0300

    added some js functionality opening the modal , closing it using the button and closing it when you click on the overlay

--- modal.js
+++ modal.js
@@ -3,3 +3,42 @@ const modalOverlay = document.querySelectorAll(".modal");
 const modalContent = document.querySelectorAll(".modal-content");
 const learnMoreBtn = document.querySelectorAll(".learn");
 const closeBtn = document.querySelectorAll(".close");
+
+
+
+learnMoreBtn.forEach(function(btn , i){
+
+    btn.addEventListener("click" , function(){
+        modalOverlay[i].style.display="flex";
+        modalOverlay[i].classList.add("show");
+        document.body.style.overflow = "hidden";
+     })
+
+})
+
+
+
+closeBtn.forEach(function(btn,i){
+
+    btn.addEventListener("click" ,function(){
+        modalOverlay[i].style.display="none";
+        modalOverlay[i].classList.remove("show");
+        document.body.style.overflow="auto";
+
+    })
+
+})
+
+
+
+modalOverlay.forEach(function(modal,i){
+
+    modal.addEventListener("click" , function(event){
+        if(event.target === modal ){
+            modalOverlay[i].style.display="none";
+            modalOverlay[i].classList.remove("show");
+            document.body.style.overflow="auto";
+        }
+    })
+})
+

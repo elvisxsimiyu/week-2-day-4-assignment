d9539c7a3376a50ee8e29beb4209fed1df764f25
Elvis Simiyu <simiyu66@gmail.com>
2026-09-15 15:32:04 +0300

    written the html , css and now working on the javascript

---
+++ modal.js
@@ -0,0 +1,5 @@
+//picking elements
+const modalOverlay = document.querySelectorAll(".modal");
+const modalContent = document.querySelectorAll(".modal-content");
+const learnMoreBtn = document.querySelectorAll(".learn");
+const closeBtn = document.querySelectorAll(".close");
